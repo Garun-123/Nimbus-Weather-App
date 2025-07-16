2c711f89cbd6b828e192dafe74bd3b052d88e4e7
GarunJad <garunjadaun@gmail.com>
2025-07-16 15:09:01 +0530

    Add functionality of imageurl to attach the icons in website

--- src/weather.js
+++ src/weather.js
@@ -37,6 +37,7 @@ async function forecast(event)
     Temperature.textContent="Present Temperature: "+data.currentConditions.temp+"°F";
     
     let new_btn=document.createElement("button");
+    new_btn.classList.add("celsius");
     new_btn.textContent="Change to Celsius";
     
     new_btn.addEventListener("click",()=>{
@@ -45,24 +46,32 @@ async function forecast(event)
         let temp_Celsius=changetoCelsius(data.currentConditions.temp);
         Temperature.textContent="Present Temperature: "+temp_Celsius.toFixed(2)+"°C";
         new_btn.textContent="Change to Fahrenheit";
+        
         Temperature.append(new_btn);
       }
       else
       {
         Temperature.textContent="Present Temperature: "+data.currentConditions.temp+"°F";
         new_btn.textContent="Change to Celsius";
+        
         Temperature.append(new_btn);
 
       }
     
     })
-       dailyforecast(city);
-       futureforecast(city); 
+      
     new_btn.style.marginLeft="10px";
     Temperature.append(new_btn);
 
     let rain_prob=document.querySelector(".rain-probability");
     rain_prob.textContent="Rain Probability: "+data.currentConditions.precipprob+"%"; 
+
+    let img=document.querySelector(".present-icon");
+    let icon=data.currentConditions.icon;
+    img.src=imageurl(icon);
+       dailyforecast(city);
+       futureforecast(city);
+        airCondition(city); 
 }
 
 function changetoCelsius(temp)
@@ -109,7 +118,10 @@ async function dailyforecast(data) {
     let time_dom = document.querySelector(`.time-${i}`);
     time_dom.textContent = new_time; 
     new_time=" ";
-
+    let iconCode = response.days[0].hours[6 + 3 * i].icon;
+    let iconUrl = imageurl(iconCode);
+    document.querySelector(`#icon-${i}`).src = iconUrl;
+    document.querySelector(`#icon-${i}`).alt = iconCode;
     let temp = document.querySelector(`.celsius-${i}`);
     temp.textContent = changetoCelsius(response.days[0].hours[6 + 3 * i].temp).toFixed(2) + "°C";
 
@@ -118,6 +130,48 @@ async function dailyforecast(data) {
   }
 }
 
+function imageurl(iconCode) {
+  if (iconCode === "clear-day") {
+    return "https://cdn-icons-gif.flaticon.com/17102/17102813.gif";
+  } 
+  else if (iconCode === "clear-night") {
+    return "https://cdn-icons-gif.flaticon.com/15585/15585721.gif";
+  }
+  else if (iconCode === "partly-cloudy-day") {
+    return "https://cdn-icons-gif.flaticon.com/17102/17102931.gif";
+  }
+  else if (iconCode === "partly-cloudy-night") {
+    return "https://cdn-icons-gif.flaticon.com/6455/6455043.gif";
+  }
+  else if (iconCode === "cloudy") {
+    return "https://cdn-icons-gif.flaticon.com/17102/17102874.gif";
+  }
+  else if (iconCode === "rain") {
+    return "https://cdn-icons-gif.flaticon.com/17102/17102963.gif";
+  }
+  else if (iconCode === "snow") {
+    return "https://cdn-icons-gif.flaticon.com/19003/19003498.gif";
+  }
+  else if (iconCode === "sleet") {
+    return "https://cdn-icons-png.flaticon.com/128/2204/2204342.png";
+  }
+  else if (iconCode === "wind") {
+    return "https://cdn-icons-gif.flaticon.com/8793/8793300.gif";
+  }
+  else if (iconCode === "fog") {
+    return "https://cdn-icons-gif.flaticon.com/6454/6454995.gif";
+  }
+  else if (iconCode === "hail") {
+    return "https://cdn-icons-gif.flaticon.com/17858/17858185.gif";
+  }
+  else if (iconCode === "thunderstorm") {
+    return "https://cdn-icons-gif.flaticon.com/17858/17858208.gif";
+  }
+  else {
+    
+    return "https://cdn-icons-gif.flaticon.com/18998/18998592.gif";
+  }
+}
 function name_days(index)
 {
   let date = new Date();
@@ -136,11 +190,28 @@ async function futureforecast(data)
    day.textContent=name_days(i);
    let temp_Variation=document.querySelector(`.min-max-${i+1}`);
    temp_Variation.textContent=changetoCelsius(response.days[i].tempmax).toFixed(2)+"°C/"+changetoCelsius(response.days[i].tempmin).toFixed(2)+"°C";
+   let icon=document.querySelector(`#new-icon-${i+1}`);
+   icon.src=imageurl(response.days[i].icon);
   }
 }
+
+async function airCondition(data)
+{
+  let response=await fetchtemp(data);
+  let feels_like=document.querySelector(".air-condition>.feels-like");
+  feels_like.textContent="feels-like: "+response.currentConditions.feelslike;
+  let wind=document.querySelector(".air-condition>.wind");
+  wind.textContent="Wind-speed: "+response.currentConditions.windspeed;
+  let humidity=document.querySelector(".air-condition>.humidity");
+  humidity.textContent="Humidity: "+response.currentConditions.humidity;
+  let uv=document.querySelector(".air-condition>.uv-index");
+  uv.textContent="UV-index: "+response.currentConditions.uvindex;
+}
+
 forecast(null);
 dailyforecast("Noida"); 
 futureforecast("Noida");
+airCondition("Noida");
 
 
 
